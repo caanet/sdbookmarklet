b9100d8048fc8cef0ae7cd0cf7353807d6a44758
Victor Cao <code@victorcao.com>
2023-01-12 13:00:52 -0800

    SD Agenda Comment initial file

---
+++ sdagendacomments.js
@@ -0,0 +1,7 @@
+// Autofill essential information for San Diego City's Agenda Comment Form
+$('#edit-submitted-comment-type-1').attr('checked',true);
+$('#edit-submitted-meeting-date-month>option:eq(2)').attr('selected',true);
+$('#edit-submitted-meeting-date-day>option:eq(21)').attr('selected',true);
+$('#edit-submitted-meeting-date-year>option:eq(3)').attr('selected',true);
+$('#edit-submitted-agenda-item-number-only').val('187');
+$('#edit-submitted-comment').val('I strongly oppose item 187, which will allow bad actors to extort property owners and disrupt neighboring tenants.');
