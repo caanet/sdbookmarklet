f0965497dcd18660ab3b9e832ba8de424202c32b
Victor Cao <victortcao@gmail.com>
2023-01-12 12:59:04 -0800

    BookmarkletJS
    This is the bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,2 @@
+// use this code as your href attribute on your 'Install this bookmarklet' anchor tag.
+javascript:(function(){var jsCode = document.createElement('script');jsCode.setAttribute('src', 'http://example.com/path/to/file.js');document.body.appendChild(jsCode);}());
